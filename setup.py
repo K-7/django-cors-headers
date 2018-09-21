54101c8b8ee2e20eb816c33107bb0290d996b348
Kenith Aiyappa <kenith.777@gmail.com>
2018-09-21 11:48:06 +0530

    Adding support to Django 1.6

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
